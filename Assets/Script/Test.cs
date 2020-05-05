1cb579452a2d47841d376d030056191731aded1d
SwillPudding <swillpudding.mm@gmail.com>
2020-05-05 21:08:58 +0900

    test

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var test = "TEST!!";
+        Debug.Log(test);
+        Debug.Log(test.Length);
+
+        test = null;
+        Debug.Log(test.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
